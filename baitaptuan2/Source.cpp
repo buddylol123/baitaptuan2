d9c014dcad7a6b4fa66c64386e503dc3b827e6c0
trunghuyen99 <mhthuyen.c3tqcap.a10@gmail.com>
2020-10-13 10:54:35 +0700

    update 2

---
+++ baitaptuan2/Source.cpp
@@ -0,0 +1,24 @@
+#include "Header.h"
+
+float phepToanCoBan(float a, float b)
+{
+	int chon;
+	cout << "Nhap a: ";
+	cin >> a;
+	cout << "Nhap b: ";
+	cin >> b;
+	cout << "Chon cong thuc: ";
+	cin >> chon;
+	switch (chon)
+	{
+	case 1:
+			cout << a + b << endl;
+	case 2:
+			cout << a - b << endl;
+	case 3:
+			cout << a * b << endl;
+	case 4:
+			cout << a / b << endl;
+	}
+	return 1;
+}
