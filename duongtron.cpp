3df3d876be409e3a39252212d180b9e1370e50ce
Loi <thanhloi486@gmail.com>
2020-10-20 00:22:37 +0700

    merge and fixed hinh tron

--- duongtron.cpp
+++ duongtron.cpp
@@ -1,33 +1,23 @@
-#include"toan cap 1.h"
-
-
-float TinhChuviHCN(float dai, float rong)
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
 {
-	float cv = (dai + rong) * 2;
-	return cv;
-}
-float TinhDientichHCN(float dai, float rong)
-{
-	float dt = dai * rong;
-	return dt;
-}
-float TinhChuviVuong(float canh)
-{
-	float cvhv = canh * 4;
-	return cvhv;
-}
-float TinhDientichVuong(float canh)
-{
-	float dthv = canh * canh;
-	return dthv;
-}
-float TinhChuviTron(float r)
-{
-	float cvht = (2 * r) * 3.14;
-	return cvht;
-}
-float TinhDientichTron(float r)
-{
-	float dtht = (r * r) * 3.14;
-	return dtht;
+   int circle_radius;
+   float PI_VALUE=3.14, circle_area, circle_circumf;
+ 
+   //Yêu cầu người dùng nhập vào bán kính
+   cout<<"Nhập vào bán kính R: ";
+   cin>>circle_radius;
+ 
+   //tính diện tích
+   circle_area = PI_VALUE * circle_radius * circle_radius;
+   cout<<"Diện tích hình tròn là: "<<circle_area;
+ 
+   //tính chu vi
+   circle_circumf = 2 * PI_VALUE * circle_radius;
+   cout<<"\nChu vi hình tròn là: "<<circle_circumf;
+    
+    cout<<"\n--------------------------------\n";
+    cout<<"Chương trình này được đăng tại Freetuts.net";
 }
